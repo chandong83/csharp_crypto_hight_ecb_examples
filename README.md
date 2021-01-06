d279fb56f70b80dd503d33b34fc8a3d62d0354fe
chandong83 <chandong83@naver.com>
2021-01-06 14:38:25 +0900

    first

---
+++ README.md
@@ -0,0 +1,13 @@
+# CRYPTO HIGHT ECB EXAMPLE for C# (C Sharp)
+
+
+## result screen
+
+![](pic/pic1.png)    
+
+
+
+
+## Reference link
+
+https://seed.kisa.or.kr/kisa/Board/18/detailView.do
---
+++ crypto_hight_ecb_example/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CryptoHIGHT;
+namespace crypto_hight_ecb_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CryptoHIGHT.ecb ecb = new CryptoHIGHT.ecb();
+            ecb.test();
+
+            Console.Write("Press any key to close");
+            Console.ReadKey();
+        }
+    }
+}
